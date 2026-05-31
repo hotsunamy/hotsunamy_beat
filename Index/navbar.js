5df0e2d7d1f93f3316528cd9d9ac5a9e612cd5f1
hotsunamy <quangminh992000@gmail.com>
2026-06-01 03:38:53 +0700

    Add files via upload

---
+++ Index/navbar.js
@@ -0,0 +1,17 @@
+document.body.insertAdjacentHTML('afterbegin', `
+    <ul>
+    <li><a href="home.html">Home</a></li>
+    <li><a href="beat.html">Beat</a></li>
+    <li><a href="mix_master.html">Mix / Master</a></li>
+    </ul>
+    <div class="container">
+    <img class="avatar" src="image.jpg" alt="img">
+    <h1>HOTSUNAMY BEATS</h1>
+    <p>Chuyên dịch vụ mix / master, phối beat các loại</p>
+    </div>
+
+`)
+
+document.body.insertAdjacentHTML('beforeend', `
+    <p class="lienhe">Liên hệ: 069 6969 6969</p>
+`)
